36e33e976625ee537973697620cc3eee8273713c
amitpatidar-droid <amit.patidar@digiprima.co>
2026-06-05 11:33:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send(`
     <h1>NodeJS App Deployed using GitHub Actions + Terraform</h1>
-    <p>CI/CD Pipeline Working Successfully 🚀</p>
+    <p>CI/CD Pipeline Working Successfully Created by DevOps Team🚀</p>
   `);
 });
 
